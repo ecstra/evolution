42a3bd3fa1e4124d1bd0a155670112610ebaaac7
Gautam S <gotham47g@gmail.com>
2025-11-24 15:39:07 +0530

    Updated Live Demo Link

--- simulation-ui/vite.config.ts
+++ simulation-ui/vite.config.ts
@@ -5,6 +5,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/evolve/',
   plugins: [
     react(),
     wasm(),
